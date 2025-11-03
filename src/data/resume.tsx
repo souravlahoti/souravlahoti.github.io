3bc65de6ed433f40f1cd9682853aa5f5636e12ff
souravlahoti <sourav26721062@gmail.com>
2025-11-03 17:40:50 +0530

    removed blog

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -39,7 +39,7 @@ export const DATA = {
   ],
   navbar: [
     { href: "/", icon: HomeIcon, label: "Home" },
-    { href: "/blog", icon: NotebookIcon, label: "Blog" },
+    // { href: "/blog", icon: NotebookIcon, label: "Blog" },
   ],
   contact: {
     email: "sourav26721062@gmail.com",
